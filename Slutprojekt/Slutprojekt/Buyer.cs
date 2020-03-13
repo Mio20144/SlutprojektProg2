e16664356846f8dcfd7365584a6421317624da2d
Olle Hasselstrom <olle.choukrihasselstrom@elev.ga.ntig.se>
2020-03-13 13:30:53 +0100

    la till i buyer

--- Slutprojekt/Slutprojekt/Buyer.cs
+++ Slutprojekt/Slutprojekt/Buyer.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Slutprojekt
 {
-    class Buyer
+    class Buyer : Customer
     {
+        Util u = new Util();
+        public Buyer()
+        {
+            int inventorySpace = u.generator.Next(1, 4);
+            int intelligence = u.generator.Next()
+        }
+
+        
+
     }
 }
